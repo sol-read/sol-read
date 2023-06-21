2d10fb4910dcf8570f53d4657eb9edfd60f6df1c
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-06-21 16:51:02 +0100

    Shaping

--- MegansPantry/src/main/java/com/solread/meganspantry/MegansPantryApplication.java
+++ MegansPantry/src/main/java/com/solread/meganspantry/MegansPantryApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MegansPantryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MegansPantryApplication.class, args);
+
 	}
 
 }
