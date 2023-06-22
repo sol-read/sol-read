a171ae1237a445cd52cf73b17c596cc926d85e4c
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-06-22 10:59:09 +0100

    Two basic repo functions to access veggie & vegan ingredients

--- MegansPantry/src/main/java/com/solread/meganspantry/repository/IngredientRepository.java
+++ MegansPantry/src/main/java/com/solread/meganspantry/repository/IngredientRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.solread.meganspantry.model.Ingredient;
 
+import java.util.List;
+
 public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
+
+    public List<Ingredient> findByVeganTrue();
+    public List<Ingredient> findByVegetarianTrue();
 
 }
