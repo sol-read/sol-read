d3997dfcbaf1651d1becc15d00594e8f99511cd2
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-06-23 17:08:30 +0100

    Initial commit of the data to inject into the H2 database. Will add to this.

---
+++ MegansPantry/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into ingredient (name, unit, vegetarian, vegan) values ('salt', 'GRAMS',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('pepper', 'GRAMS',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('onion', 'HALVES',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('garlic', 'CLOVES',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('eggs', 'NONE',true, false);
+insert into ingredient (name, unit, vegetarian, vegan) values ('milk', 'MILLILITRES',true, false);
+insert into ingredient (name, unit, vegetarian, vegan) values ('bell peppers', 'NONE',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('chicken breast', 'NONE',false, false);
+insert into ingredient (name, unit, vegetarian, vegan) values ('black beans', 'TINS',true, true);
+insert into ingredient (name, unit, vegetarian, vegan) values ('tomatoes', 'NONE', true, true);
