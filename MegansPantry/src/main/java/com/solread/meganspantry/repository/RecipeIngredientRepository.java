d39c207d76d322c3872f9354039349e771be0b90
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-06-26 11:46:03 +0100

    Created recipe ingredient repo - not sure if it'll find much use but worth creating.

---
+++ MegansPantry/src/main/java/com/solread/meganspantry/repository/RecipeIngredientRepository.java
@@ -0,0 +1,8 @@
+package com.solread.meganspantry.repository;
+
+import com.solread.meganspantry.model.RecipeIngredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeIngredientRepository extends CrudRepository<RecipeIngredient, Integer> {
+
+}
