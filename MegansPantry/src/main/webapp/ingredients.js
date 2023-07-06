84d1fe0b0463f0d632f89a25b0eefb14cbc1515c
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-06 15:20:25 +0100

    Including the updates to other .js scripts

---
+++ MegansPantry/src/main/webapp/ingredients.js
@@ -0,0 +1,57 @@
+const fetchAllIngredients = (cb) => {
+    fetch("/ingredients/all")
+    .then(res => res.json())
+    .then(handle500Error)
+    .then(json => cb(json))
+    .catch(renderError);
+}
+
+const addNewIngredient = (cb) => {
+    const addIngredientForm = document.getElementById("addNewIngredient").elements;
+    const name = addIngredientForm["name"].value;
+    const unit = addIngredientForm["unit"].value;
+    const vegetarian = addIngredientForm["vegetarian"].value;
+    const vegan = addIngredientForm["vegan"].value;
+    const boot = {
+      name,
+      unit,
+      vegetarian,
+      vegan
+    }
+    fetch("/ingredients/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ingredient)
+    })
+      .then(res => res.json())
+      .then(handle500Error)
+      .then(json => {
+        alert(`Successfully added ingredient with id ${json.id}: (${JSON.stringify(json)})`);
+        fetchAllIngredients(cb);
+      })
+      .catch(renderError);
+  }
+
+
+  const renderIngredientsListCallback = (ingredientsTableBody) => (boots) => {
+    while (ingredientsTableBody.firstChild) {
+      ingredientsTableBody.removeChild(ingredientsTableBody.firstChild);
+    }
+    ingredients.forEach((ingredient) => {
+      const ingredientsRow = document.createElement("tr");
+      ingredientsRow.innerHTML = `
+        <td>${ingredient.id}</td>
+        <td>${ingredient.name}</td>
+        <td>${ingredient.unit}</td>
+        <td>${ingredient.vegetarian}</td>
+        <td>${ingredient.vegan}</td>
+      `;
+      ingredientsTableBody.appendChild(ingredientsRow);
+    });
+  }
+
+  fetchAllIngredients(
+    renderIngredientsListCallback(
+      document.getElementById("ingredientsTableBody")
+    )
+  );
--- MegansPantry/src/main/webapp/recipes.js
+++ MegansPantry/src/main/webapp/recipes.js
@@ -12,3 +12,25 @@ const fetchAllRecipes = (cb) => {
     .then(json => cb(json))
     .catch(renderError);
 }
+
+const addNewRecipe = (cb) => {
+  const addRecipeForm = document.getElementById("addNewRecipe").elements;
+  const name = addRecipeForm["name"].value;
+  const type = addRecipeForm[""].value;
+  const boot = {
+    name,
+    ingredients
+  }
+  fetch("/recipes/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(boot)
+  })
+    .then(res => res.json())
+    .then(handle500Error)
+    .then(json => {
+      alert(`Successfully added boot with id ${json.id}: (${JSON.stringify(json)})`);
+      fetchAllBoots(cb);
+    })
+    .catch(renderError);
+}
