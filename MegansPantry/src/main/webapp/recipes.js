8f3368c7ed8183b9d3d3ead2cb0a567399b853a3
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-12 18:05:14 +0100

    Putting together the start of a function to display the ingredients in a table for a clicked recipe.

--- MegansPantry/src/main/webapp/recipes.js
+++ MegansPantry/src/main/webapp/recipes.js
@@ -19,6 +19,23 @@ function fetchData(apiEndpoint) {
     });
 }
 
+function showIngredients(ingredients) {
+    const recipeIngredientBody = document.getElementById("ingredientForRecipeBody");
+    recipeIngredientBody.innerHTML = "";
+
+    ingredients.forEach((ingredient) => {
+        const ingredientItem = document.createElement("tr");
+        ingredientItem.innerHTML = `
+        <td>${ingredient.id}</td>
+        <td>${ingredient.name}</td>
+        <td>${ingredient.amountNeeded}</td>
+        <td>${ingredient.unit}</td>
+        <td>${ingredient.amountInPantry}</td>
+        `;
+        recipeIngredientBody.appendChild(ingredientItem);
+    });
+}
+
 function populateTableWithRecipes(recipes) {
   const tableBody = document.getElementById("recipesTableBody");
   tableBody.innerHTML = "";
@@ -27,11 +44,23 @@ function populateTableWithRecipes(recipes) {
     const row = document.createElement("tr");
     row.innerHTML = `
       <td>${recipe.id}</td>
-      <td><a href="/recipes/${recipe.id}/ingredients">${recipe.name}</a></td>
+      <td><a href="#recipeIngredientSection" class="recipe-link">${recipe.name}</a></td>
       <td>${recipe.vegetarian}</td>
       <td>${recipe.vegan}</td>
     `;
     tableBody.appendChild(row);
+
+    const recipeLink = row.querySelector(".recipe-link");
+    recipeLink.addEventListener('click', () => {
+        fetchData(`/recipes/${recipe.id}/ingredients`)
+        .then((ingredients) => {
+            showIngredients(ingredients);
+        })
+        .catch((error) => {
+            console.error("Error displaying ingredients: ", error);
+            throw error;
+        });
+    });
   });
 }
 
