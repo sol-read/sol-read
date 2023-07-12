939c44cf8c36e60189275c4c19170cb9c009d172
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-12 12:40:28 +0100

    Removing the unused addRecipe function.

--- MegansPantry/src/main/webapp/recipes.js
+++ MegansPantry/src/main/webapp/recipes.js
@@ -9,29 +9,6 @@ const renderError = (message) => {
   alert(`Error calling Ingredients API: ${message}`);
 }
 
-// const addNewRecipe = (cb) => {
-//   const addRecipeForm = document.getElementById("addNewRecipe").elements;
-//   const name = addRecipeForm["name"].value;
-//   const vegetarian = addRecipeForm["vegetarian"].value;
-//   const vegan = addRecipeForm["vegan"].value;
-//   const recipe = {
-//     name,
-//     ingredients
-//   }
-//   fetch("/recipes/add", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(boot)
-//   })
-//     .then(res => res.json())
-//     .then(handle500Error)
-//     .then(json => {
-//       alert(`Successfully added boot with id ${json.id}: (${JSON.stringify(json)})`);
-//       fetchAllBoots(cb);
-//     })
-//     .catch(renderError);
-// }
-
 function fetchData(apiEndpoint) {
   return fetch(apiEndpoint)
     .then((response) => response.json())
