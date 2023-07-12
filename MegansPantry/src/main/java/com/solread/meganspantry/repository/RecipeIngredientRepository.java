385e1e5dfaf63bede7c2a525239299cd2f56d469
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-12 17:46:29 +0100

    Added find function to find the amounts for a given recipe.

--- MegansPantry/src/main/java/com/solread/meganspantry/repository/RecipeIngredientRepository.java
+++ MegansPantry/src/main/java/com/solread/meganspantry/repository/RecipeIngredientRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RecipeIngredientRepository extends CrudRepository<RecipeIngredientAmount, Integer> {
+
     List<RecipeIngredientAmount> findAll();
 
+    List<RecipeIngredientAmount> findByRecipeId(Integer recipeId);
     RecipeIngredientAmount findByRecipeIdAndIngredientId(Integer recipeId, Integer ingredientId);
 }
