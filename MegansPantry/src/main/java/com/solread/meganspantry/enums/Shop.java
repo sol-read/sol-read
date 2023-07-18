6476a3e344a145590d3b14d2011f4bf04c60e04a
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-18 16:19:41 +0100

    Shop enum

---
+++ MegansPantry/src/main/java/com/solread/meganspantry/enums/Shop.java
@@ -0,0 +1,17 @@
+package com.solread.meganspantry.enums;
+
+public enum Shop {
+
+    WAITROSE,
+    MARKS_AND_SPENCERS,
+    TESCO,
+    ASDA,
+    SAINSBURYS,
+    COOP,
+    MORRISONS,
+    LIDL,
+    ALDI,
+    OCADO,
+    OTHER
+
+}
