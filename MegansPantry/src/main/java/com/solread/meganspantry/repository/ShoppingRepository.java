ec0b0f002e3ffc40297e5f5e3c65d95048368276
Sol Read <90840032+sol-read@users.noreply.github.com>
2023-07-18 16:32:32 +0100

    First steps.

--- MegansPantry/src/main/java/com/solread/meganspantry/repository/ShoppingRepository.java
+++ MegansPantry/src/main/java/com/solread/meganspantry/repository/ShoppingRepository.java
@@ -1,7 +1,13 @@
 package com.solread.meganspantry.repository;
 
+import com.solread.meganspantry.enums.Supermarket;
 import com.solread.meganspantry.model.Shop;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ShoppingRepository extends CrudRepository<Shop, Integer> {
+
+    public List<Shop> findBySupermarket(Supermarket supermarket);
+
 }
